3c12d0684f65cea8263f16bfba3dc895bd2ab8d3
AnasImloul <pydev27@gmail.com>
2023-12-01 22:57:57 +0100

    optimized parser function

--- 2023/day_01/second_part.cpp
+++ 2023/day_01/second_part.cpp
@@ -13,36 +13,44 @@
 std::string digits[18] = {
         "one","two", "three",
         "four", "five", "six",
-        "seven", "eight", "nine",
-        "1", "2", "3",
-        "4", "5", "6",
-        "7", "8", "9"
+        "seven", "eight", "nine"
 };
 
 namespace {
-    bool isNext(const std::string& line, const std::string& substring, int start = 0) {
+    int longestMatch(const std::string& line, const std::string& substring, int start = 0) {
         int j = 0;
         while (j < substring.size() && start + j < line.size() && substring[j] == line[start + j]) {
             j++;
         }
-        return j == substring.size();
+        return j;
     }
 
     int parse(const std::string& line) {
-        int left = -1, right = -1;
+        std::pair<int, int> left = {-1, INT_MAX}, right = {-1, INT_MIN};
+        for (int d = 1; d <= 9; d++) {
+            std::string digit = digits[d - 1];
+            int i = 0;
+            while (i < line.size()) {
+                int j = longestMatch(line, digit, i);
+                if (j == digit.size()) {
+                    if (left.second  > i) left = {d, i};
+                    if (right.second < i) right = {d, i};
+                }
+                if (j == 0) i++;
+                else i += j;
+            }
+        }
+
         int i = 0;
         while (i < line.size()) {
-            for (int d = 0; d < 18; d++) {
-                std::string digit = digits[d];
-                if (isNext(line, digit, i)) {
-                    if (left == -1) left = d % 9 + 1;
-                    right = d % 9 + 1;
-                    break;
-                }
+            if (isdigit(line[i])) {
+                if (left.second > i) left = {line[i] - '0', i};
+                if (right.second < i) right = {line[i] - '0', i};
             }
             i++;
         }
-        return left * 10 + right;
+
+        return left.first * 10 + right.first;
     }
 }
 
