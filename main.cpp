4707a40b9ba50da0620f64f16de8343a45e76750
AnasImloul <pydev27@gmail.com>
2023-12-03 16:40:00 +0100

    tested day_03 problem in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <chrono>
-#include "2023/day_02/cube_conundrum.h"
+#include "2023/day_03/gear_ratios.h"
 
 int64_t time() {
     using namespace std::chrono;
@@ -9,7 +9,7 @@ int64_t time() {
 
 int main() {
     int64_t start = time();
-    std::cout << cubeConundrum::firstPart() << std::endl;
+    std::cout << gearRatios::secondPart() << std::endl;
     std::cout << time() - start << "ms" << std::endl;
     return 0;
 }
