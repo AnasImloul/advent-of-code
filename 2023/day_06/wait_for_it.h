892bf25f51208ab199a1c408c8c20f3b81fba914
AnasImloul <pydev27@gmail.com>
2023-12-06 06:42:33 +0100

    Fixed typo in comment

--- 2023/day_06/wait_for_it.h
+++ 2023/day_06/wait_for_it.h
@@ -22,9 +22,9 @@ namespace waitForIt {
     namespace {
         int winningRecharges(ll time, ll distance) {
             int winning = 0;
-            // there is plenty of room to optimizations
-            // the inequality can be seen as finding where a f(t) = t * (time - t) - distance is positive
-            // hint: find the root of a second degree polynomial
+            // there is plenty of room for optimizations
+            // the inequality can be seen as finding where this function f(t) = t * (time - t) - distance is positive
+            // hint: find the roots of a second degree polynomial
             // however the current implementation is cleaner, so i'll leave it as it is
             for (ll t = 0; t <= time; t++) {
                 if (t * (time - t) >= distance) winning++;
