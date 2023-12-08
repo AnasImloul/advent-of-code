426b1125e16a9dc983b81db70998c2e89a60043a
AnasImloul <pydev27@gmail.com>
2023-12-08 14:20:55 +0100

    Refactored the code

--- 2023/day_08/haunted_wasteland.h
+++ 2023/day_08/haunted_wasteland.h
@@ -20,41 +20,27 @@ namespace hauntedWasteland {
     namespace {
         using Network = std::unordered_map<std::string, std::pair<std::string, std::string>>;
         using StartingNodesExtractor = std::vector<std::string>(Network&);
+
         std::array<std::string, 3> parseNode(std::string& line) {
-
             std::array<std::string, 3> result;
-
             int i = 0;
             for (int j = 0; j < 3; j++) {
                 utils::skipUntil(line, i, utils::isalnum);
                 result[j] = utils::readWhile(line, i, utils::isalnum);
             }
-
             return result;
         }
 
-        Network parseNetwork(std::vector<std::string>& lines) {
+        ll solve(StartingNodesExtractor extractStartingNodes) {
+            std::string instructions = utils::readLine();
+            utils::readLine();
+
             Network network;
-            for (std::string& line: lines) {
+            while (!std::cin.eof()) {
+                std::string line = utils::readLine();
                 auto [node, left, right] = parseNode(line);
                 network[node] = {left, right};
             }
-            return network;
-        }
-
-        std::pair<std::string, Network> readInput() {
-            std::string instructions = utils::readLine();
-
-            utils::readLine();
-
-            std::vector<std::string> lines = utils::readLines();
-            Network network = parseNetwork(lines);
-
-            return {instructions, network};
-        }
-
-        ll solve(StartingNodesExtractor extractStartingNodes) {
-            auto [instructions, network] = readInput();
 
             std::vector<std::string> nodes = extractStartingNodes(network);
 
@@ -70,10 +56,8 @@ namespace hauntedWasteland {
                     }
                     step++;
                 } while (current.back() != 'Z');
-
                 answer = std::lcm(answer, step);
             }
-
             return answer;
         }
     }
